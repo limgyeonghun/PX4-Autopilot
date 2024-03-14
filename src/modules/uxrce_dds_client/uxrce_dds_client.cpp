cf2cfc3aa5eb5716ee7a0829871f761b32ae8511
limgyeonghun <dladks123@gmail.com>
2024-03-14 08:07:20 +0000

    Create topic namespace based on sys_id

--- src/modules/uxrce_dds_client/uxrce_dds_client.cpp
+++ src/modules/uxrce_dds_client/uxrce_dds_client.cpp
@@ -592,7 +592,15 @@ UxrceddsClient *UxrceddsClient::instantiate(int argc, char *argv[])
 	bool localhost_only = false;
 	bool custom_participant = false;
 
-	const char *client_namespace = nullptr;//"px4";
+	//const char *client_namespace = nullptr;//"px4";
+
+	int32_t sys_id;
+    param_t sysid_param = param_find("MAV_SYS_ID");
+    param_get(sysid_param, &sys_id);
+
+    char sys_id_str[50];
+    snprintf(sys_id_str, sizeof(sys_id_str), "vehicle%d", (int)sys_id);
+	const char *client_namespace = strdup(sys_id_str);
 
 	while ((ch = px4_getopt(argc, argv, "t:d:b:h:p:lcn:", &myoptind, &myoptarg)) != EOF) {
 		switch (ch) {
